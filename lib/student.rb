c186bab6e0030aaf81aed4c25ceabed87cb40a2f
Jonathan East <jonathan_east@ymail.com>
2020-07-05 18:14:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,11 @@ class Student
   end
 
   def self.create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
   end
 end
